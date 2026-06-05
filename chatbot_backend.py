f00ddbe4a73799b123f5740042973e12cc6e1f0c
prince Jha <jhaprince0407@gmail.com>
2026-06-05 21:32:18 +0530

    working master chatbot with tools support

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,56 +1,67 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
 from langchain_groq import ChatGroq
-from langchain.messages import HumanMessage
-from langgraph.graph import StateGraph, START , END 
-from pydantic import BaseModel , Field
-from typing import Annotated, TypedDict, List
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_community.tools import DuckDuckGoSearchResults
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain.tools import tool
+from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
-import  operator
-import os
+import sqlite3
+import requests
 
 load_dotenv()
 
-if "GROQ_API_KEY" not in os.environ:
-    os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+llm = ChatGroq(model="llama-3.1-8b-instant")
 
-llm = ChatGroq(model="qwen/qwen3-32b")
+##tools 
 
-#pydantic schema
-class ai_response(BaseModel):
-    response : str=Field(description="ai response on user input")
+search_tool = DuckDuckGoSearchResults(region="us-en")
 
-#structure model 
-model = llm.with_structured_output(ai_response)
+@tool
+def get_stock_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA') 
+    using Alpha Vantage with API key in the URL.
+    """
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=C9PE94QUEW9VWGFM"
+    r = requests.get(url)
+    return r.json()
 
-#state
-class chatbot(TypedDict):
-    user_input: str
-    ai_response: str
-
-
-#agent function
-def chat_with_ai(state:chatbot)->chatbot:
-
-    prompt = state['user_input']
-    
-    output = model.invoke(prompt)
-
-    return {"ai_response": [output.response]}
-    
-
-#graph building
-graph = StateGraph(chatbot)
-
-graph.add_node("chat_node",chat_with_ai)
-graph.add_edge(START,"chat_node")
-graph.add_edge("chat_node",END)
-
-chatbot = graph.compile()
-
-# response = chatbot.invoke(
-#     {"user_input":"what is 2+2"}
-# )
-
-# print(response)
+tools = [search_tool, get_stock_price]
+llm_with_tools = llm.bind_tools(tools)
 
 
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
 
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm_with_tools.invoke(messages)
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools)
+
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
+
+# Checkpointer
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node)
+
+graph.add_edge(START, "chat_node")
+graph.add_conditional_edges("chat_node",tools_condition)
+graph.add_edge("tools","chat_node")
+
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
